808570a24ee78e4fa3dc46e188bfa030f98f9e45
Ross Lawley <ross.lawley@gmail.com>
2019-12-03 10:53:55 +0000

    Close cursor after its been invalidated

--- src/main/java/com/mongodb/kafka/connect/source/MongoSourceTask.java
+++ src/main/java/com/mongodb/kafka/connect/source/MongoSourceTask.java
@@ -141,6 +141,7 @@ public class MongoSourceTask extends SourceTask {
                 // If the cursor is invalidated add the record and return calls
                 if (changeStreamDocument.getString("operationType", new BsonString("")).getValue().equalsIgnoreCase(INVALIDATE)) {
                     LOGGER.info("Cursor has been invalidated.");
+                    cursor.close();
                     cursor = null;
                     return sourceRecords;
                 } else if (sourceRecords.size() == maxBatchSize) {
